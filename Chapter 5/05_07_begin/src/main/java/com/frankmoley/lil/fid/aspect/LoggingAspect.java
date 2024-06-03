32490cd3ab1d3948f11f5addbdca85c052cebbbe
George Panaretos <georgepanaretos.cac@gmail.com>
2024-06-03 15:36:14 +0200

    Chapter 5.7 begin

--- Chapter 5/05_07_begin/src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
+++ Chapter 5/05_07_begin/src/main/java/com/frankmoley/lil/fid/aspect/LoggingAspect.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -21,10 +23,14 @@ public class LoggingAspect {
     @Pointcut("@annotation(Loggable)")
     public void executeLogging(){}
 
-    @AfterReturning(value = "executeLogging()", returning = "returnValue")
-    public void logMethodCall(JoinPoint joinPoint, Object returnValue){
+    @Around(value = "executeLogging()")
+    public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object returnValue = joinPoint.proceed();
+        long totalTime = System.currentTimeMillis()-startTime;
         StringBuilder message = new StringBuilder("Method: ");
         message.append(joinPoint.getSignature().getName());
+        message.append(" totalTime: ").append(totalTime).append("ms");
         Object[] args = joinPoint.getArgs();
         if (null!=args && args.length>0){
             message.append(" args=[ | ");
@@ -40,6 +46,7 @@ public class LoggingAspect {
         }
 
         LOGGER.info(message.toString());
+        return returnValue;
     }
 
 
